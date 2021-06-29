91fad5d70ea0eac8d19ce8b112db53dae5867e40
BenFrancka <benfrancka@gmail.com>
2021-06-29 12:30:35 -0700

    adds seed data to todos.js, renames env-example to env

---
+++ data/todos.js
@@ -0,0 +1,23 @@
+module.exports = [
+  {
+    todo: 'walk the dog',
+    completed: false,
+  },
+  {
+    todo: 'wash the dishes',
+    completed: false,
+  },
+  {
+    name: 'complete daily code challenge',
+    completed: false,
+  },
+  {
+    name: 'understand regex',
+    completed: false,
+  },
+  {
+    name: 'congratulate yourself',
+    completed: false,
+  }
+
+];
